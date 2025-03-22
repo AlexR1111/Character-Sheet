4b9b9f2ed00935d8973ded1ec39e2cd4d47b60cd
AlexR1111 <148652561+AlexR1111@users.noreply.github.com>
2025-03-22 15:24:07 +0100

    fixing 22.03
    adding text to aligment and race, making it work on pc

--- charactersheet/src/App.jsx
+++ charactersheet/src/App.jsx
@@ -12,7 +12,7 @@ function App() {
   return (
     <>
       <div>
-        <h1>Charactersheet 0.1</h1>
+        <h1>Charactersheet 0.2</h1>
         <Name />
         <Aligment/>
         <Race/>
--- charactersheet/src/component/Ability Score.jsx
+++ charactersheet/src/component/Ability Score.jsx
@@ -2,25 +2,25 @@ import React from "react";
 
 function AbilityScore(){
     return(
-        <div class="scoreAbility">
+        <div className="scoreAbility">
             <h2>Stats</h2>
-            <label for="sc1">Strength</label>
-            <input class="sc1" type="number" min={6} max={30} ></input><input readOnly></input>
+            <label htmlFor="sc1">Strength</label>
+            <input className="sc1" type="number" min={6} max={30} ></input><input readOnly></input>
             <br />
-            <label for="sc2">Dexterity</label>
-            <input class="sc2" type="number" min={6} max={30} ></input><input readOnly></input>
+            <label htmlFor="sc2">Dexterity</label>
+            <input className="sc2" type="number" min={6} max={30} ></input><input readOnly></input>
             <br />
-            <label for="sc3">Constitution</label>
-            <input class="sc3" type="number" min={6} max={30} ></input><input readOnly></input>
+            <label htmlFor="sc3">Constitution</label>
+            <input className="sc3" type="number" min={6} max={30} ></input><input readOnly></input>
             <br />
-            <label for="sc4">Intelligence</label>
-            <input class="sc4" type="number" min={6} max={30}></input><input readOnly></input>
+            <label htmlFor="sc4">Intelligence</label>
+            <input className="sc4" type="number" min={6} max={30}></input><input readOnly></input>
             <br />
-            <label for="sc5">Widsom</label>
-            <input class="sc5" type="number" min={6} max={30} ></input><input readOnly></input>
+            <label htmlFor="sc5">Widsom</label>
+            <input className="sc5" type="number" min={6} max={30} ></input><input readOnly></input>
             <br />
-            <label for="sc6">Charisma</label>
-            <input class="sc6" type="number" min={6} max={30} ></input><input readOnly></input>
+            <label htmlFor="sc6">Charisma</label>
+            <input className="sc6" type="number" min={6} max={30} ></input><input readOnly></input>
             
         </div>
     )
