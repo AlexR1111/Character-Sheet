b580726de7978f6148b6a0489e7460b0e04e82f2
AlexR1111 <148652561+AlexR1111@users.noreply.github.com>
2025-04-07 15:43:55 +0200

    Create Dice.jsx

---
+++ charactersheet/src/component/Dice.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+
+function Dice(){
+    let GAB = 5;
+    let mStr= 2;
+    let mDex=1;
+
+        function rangeAttack(){
+        const rangeAT = GAB + mDex;
+        return rangeAT;
+        }
+
+        function meeleAttack(){
+        const meeleAt = GAB + mStr;
+        return meeleAt;
+        }
+    
+        const attackBonusR = rangeAttack();
+        console.log("Range Attack Bonus:", attackBonusR);
+
+        const attackBonusM = meeleAttack();
+        console.log("Meele Attack Bonus:", attackBonusM);
+
+        function D20(min = 1,max = 20){
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+        }
+
+    const DiceRoll20 = D20();
+    console.log("Würfel einen D20:",DiceRoll20);
+
+    function EndAttackM(){
+        const NA = meeleAttack() + D20();
+        return NA;
+    }
+    const NahAngriff = EndAttackM();
+
+    console.log("Angriff im Nahkampf:", NahAngriff)
+
+    function EndAttackR(){
+        const FA = rangeAttack() + D20();
+        return FA;
+    }
+    const FerAngriff = EndAttackR();
+    console.log("Angriff im Fernkampf:", FerAngriff)
+    }
+
+export default Dice:
